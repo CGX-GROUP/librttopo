b838e8fa4fbd082a208139c77d83d295377dea9a
Sandro Santilli <strk@keybit.net>
2016-04-19 12:29:24 +0200

    Update homepage to use osgeo/gogs

--- src/measures3d.c
+++ src/measures3d.c
@@ -1,7 +1,7 @@
 /**********************************************************************
  *
  * rttopo - topology library
- * http://gitlab.com/rttopo/rttopo
+ * http://git.osgeo.org/gogs/rttopo/librttopo
  *
  * rttopo is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -28,7 +28,7 @@
 /**********************************************************************
  *
  * rttopo - topology library
- * http://gitlab.com/rttopo/rttopo
+ * http://git.osgeo.org/gogs/rttopo/librttopo
  * Copyright 2011 Nicklas Avén
  *
  * This is free software; you can redistribute and/or modify it under
--- src/rtin_twkb.c
+++ src/rtin_twkb.c
@@ -1,7 +1,7 @@
 /**********************************************************************
  *
  * rttopo - topology library
- * http://gitlab.com/rttopo/rttopo
+ * http://git.osgeo.org/gogs/rttopo/librttopo
  *
  * rttopo is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
