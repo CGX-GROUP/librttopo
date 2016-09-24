97b2e72477a4e2e0146d665310373c0221a90e73
Sandro Santilli <strk@kbt.io>
2016-09-24 14:23:44 +0200

    Do not complain about side-location conflicts if not asked to check faces

--- src/rtgeom_topo.c
+++ src/rtgeom_topo.c
@@ -2605,7 +2605,7 @@ _rtt_AddEdge( RTT_TOPOLOGY* topo,
                 newedge.edge_id, newedge.next_left, prev_right);
     if ( newedge.face_right == -1 ) {
       newedge.face_right = span.ccwFace;
-    } else if ( newedge.face_right != epan.ccwFace ) {
+    } else if ( modFace != -1 && newedge.face_right != epan.ccwFace ) {
       /* side-location conflict */
       rterror(iface->ctx, "Side-location conflict: "
               "new edge starts in face"
@@ -2617,7 +2617,7 @@ _rtt_AddEdge( RTT_TOPOLOGY* topo,
     }
     if ( newedge.face_left == -1 ) {
       newedge.face_left = span.cwFace;
-    } else if ( newedge.face_left != epan.cwFace ) {
+    } else if ( modFace != -1 && newedge.face_left != epan.cwFace ) {
       /* side-location conflict */
       rterror(iface->ctx, "Side-location conflict: "
               "new edge starts in face"
